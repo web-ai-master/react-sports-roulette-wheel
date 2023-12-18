07db86d67c32a3d438e4f9be737af58f01e93db3
jovan0901 <jovanvelickovic0901@gmail.com>
2023-12-19 01:18:16 +0800

    fix: image style

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -1,47 +1,17 @@
 import React from "react";
-import { Container, Typography, Box, Paper } from '@mui/material';
+import { Container, Typography } from '@mui/material';
 import RouletteStepper from "./components/RouletteStepper";
-import { createTheme, ThemeProvider } from '@mui/material/styles';
 
-const theme = createTheme({
-	palette: {
-		primary: {
-			main: '#556cd6', // You can choose your color
-		},
-		secondary: {
-			main: '#19857b', // You can choose your color
-		},
-		error: {
-			main: '#f44336',
-		},
-		background: {
-			default: '#fff',
-		},
-	},
-	typography: {
-		h4: {
-			fontWeight: 700, // You can adjust the weight of the font
-		},
-	},
-});
 
 const App = () => {
 
 	return (
-		<ThemeProvider theme={theme}>
-			<Container component="main" maxWidth="80%" sx={{ mt: 4 }}>
-				<Paper
-					elevation={3}
-					sx={{ p: 3, height: '90vh', borderRadius: "20px", backgroundImage: 'url(../assets/image/background.jpg)', backgroundRepeat: 'no-repeat', backgroundSize: 'cover', backgroundPosition: 'center' }}>
-					<Box display="flex" justifyContent="center" alignItems="center" marginBottom={2}>
-						<Typography variant="h4" component="h1" gutterBottom>
-							Roulette Stepper
-						</Typography>
-					</Box>
-					<RouletteStepper />
-				</Paper>
-			</Container>
-		</ThemeProvider>
+		<Container component="main" style={{ backgroundColor: "#0f1924", fontColor: "#dae2ed" }}>
+			<Typography variant="h4" component="h1" display="flex" justifyContent="center" alignItems="center" marginBottom={2}>
+				Roulette Stepper
+			</Typography>
+			<RouletteStepper />
+		</Container>
 	)
 }
 
--- front-end/src/components/GroupStep.js
+++ front-end/src/components/GroupStep.js
@@ -1,6 +1,7 @@
 import React, { useState, useEffect } from "react";
 import axios from "axios";
 import { GROUPS_API } from "../utils/api";
+import { ImageList, ImageListItem, ImageListItemBar } from "@mui/material";
 
 const GroupStep = ({ onSelectGroup, onHandleNext }) => {
     const [groups, setGroups] = useState([]);
@@ -23,21 +24,28 @@ const GroupStep = ({ onSelectGroup, onHandleNext }) => {
     return (
         <div>
             <h2>Select a Group</h2>
-            {groups.map((group) => (
-                <img
-                    key={group.id}
-                    style={{
-                        width: "200px",
-                        height: "200px",
-                        padding: "10px",
-                        marginRight: "10px",
-                        cursor: "pointer",
-                    }}
-                    onClick={() => handelSelectGroup(group)}
-                    src={group.image}
-                    alt="groupImage"
-                />
-            ))}
+            <ImageList>
+                {
+                    groups.map((group) => (
+                        <ImageListItem key={group.id}>
+                            <img
+                                key={group.id}
+                                style={{
+                                    width: "100px",
+                                    height: "100px",
+                                    padding: "10px",
+                                    marginRight: "10px",
+                                    cursor: "pointer",
+                                }}
+                                onClick={() => handelSelectGroup(group)}
+                                src={group.image}
+                                alt="groupImage"
+                            />
+                            <ImageListItemBar title={group.name} />
+                        </ImageListItem>
+                    ))
+                }
+            </ImageList>
         </div>
     );
 };
